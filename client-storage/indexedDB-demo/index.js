7d8c3754372f7731dbae339247a2803d7f949d55
Allen <allenxinli@gmail.com>
2019-04-17 15:15:21 +0800

    fix can not save data to indexedDB
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage

--- client-storage/indexedDB-demo/index.js
+++ client-storage/indexedDB-demo/index.js
@@ -8,38 +8,42 @@ let db;
 
 window.onload = function() {
   let request = window.indexedDB.open("notes_db", 1);
+
   request.onerror = function() {
     console.log("Database failed to open");
   };
+
   request.onsuccess = function() {
     console.log("Database opened succesfully");
+
     db = request.result;
+
     displayData();
   };
 
   request.onupgradeneeded = function(e) {
     let db = e.target.result;
-    let objectStore = db.createObjectStore("notes", {
-      keypath: "id",
+
+    let objectStore = db.createObjectStore("notes_os", {
+      keyPath: "id",
       autoIncrement: true
     });
-    objectStore.createIndex("title", "title", {
-      unique: false
-    });
-    objectStore.createIndex("body", "body", {
-      unique: false
-    });
+
+    objectStore.createIndex("title", "title", { unique: false });
+    objectStore.createIndex("body", "body", { unique: false });
+
     console.log("Database setup complete");
   };
 
   form.onsubmit = addData;
+
   function addData(e) {
     e.preventDefault();
-    let newItem = {
-      title: titleInput.nodeValue,
-      body: bodyInput.value
-    };
+
+    let newItem = { title: titleInput.value, body: bodyInput.value };
+
     let transaction = db.transaction(["notes_os"], "readwrite");
+
     let objectStore = transaction.objectStore("notes_os");
 
     var request = objectStore.add(newItem);
@@ -47,10 +51,13 @@ window.onload = function() {
       titleInput.value = "";
       bodyInput.value = "";
     };
+
     transaction.oncomplete = function() {
-      console.log("Transaction completed:Database modification finished.");
+      console.log("Transaction completed: database modification finished.");
+
       displayData();
     };
+
     transaction.onerror = function() {
       console.log("Transaction not opened due to error");
     };
@@ -60,9 +67,11 @@ window.onload = function() {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
     }
-    let objectStore = db.transaction("notes").objectStore("notes");
+
+    let objectStore = db.transaction("notes_os").objectStore("notes_os");
     objectStore.openCursor().onsuccess = function(e) {
       let cursor = e.target.result;
+
       if (cursor) {
         let listItem = document.createElement("li");
         let h3 = document.createElement("h3");
@@ -74,12 +83,15 @@ window.onload = function() {
 
         h3.textContent = cursor.value.title;
         para.textContent = cursor.value.body;
+
         listItem.setAttribute("data-note-id", cursor.value.id);
 
         let deleteBtn = document.createElement("button");
         listItem.appendChild(deleteBtn);
         deleteBtn.textContent = "Delete";
+
         deleteBtn.onclick = deleteItem;
+
         cursor.continue();
       } else {
         if (!list.firstChild) {
@@ -94,17 +106,18 @@ window.onload = function() {
 
   function deleteItem(e) {
     let noteId = Number(e.target.parentNode.getAttribute("data-note-id"));
-    let transaction = db.transaction(["notes"], "readwrite");
-    let objectStore = transaction.objectStore("notes");
+
+    let transaction = db.transaction(["notes_os"], "readwrite");
+    let objectStore = transaction.objectStore("notes_os");
     let request = objectStore.delete(noteId);
 
     transaction.oncomplete = function() {
       e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-      console.log("Note" + noteId + "deleted");
+      console.log("Note " + noteId + " deleted.");
 
       if (!list.firstChild) {
         let listItem = document.createElement("li");
-        listItem.textContent = "No notes stored";
+        listItem.textContent = "No notes stored.";
         list.appendChild(listItem);
       }
     };
